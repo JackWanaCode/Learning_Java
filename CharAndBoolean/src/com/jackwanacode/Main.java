3311b008c1ca4877439b29ec0508a363addde8a7
JackWanaCode <kazucuong@yahoo.com>
2019-05-15 21:56:12 +0000

    first commit

---
+++ CharAndBoolean/src/com/jackwanacode/Main.java
@@ -0,0 +1,15 @@
+package com.jackwanacode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //width of char is 16 (2bytes)
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        boolean myBoolean = false;
+        System.out.println(!myBoolean);
+    }
+}
